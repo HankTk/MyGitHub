5853ffdc09777a1b2e3db281a5d7aaf10958b130
Hidenori Takaku <hidenori@comcast.net>
2021-08-09 16:19:04 -0700

    Deleted

--- MyAccountProject/daterange-picker/demo/src/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-
-export const MATERIAL_DOCS_ROUTES: Routes = [
-  {
-    path: '', pathMatch: 'full',
-    loadChildren: () => import('./pages/homepage').then(m => m.HomepageModule)
-  },
-  {path: '**', redirectTo: ''},
-];
